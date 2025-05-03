4ef3178bdae8e61e3feff3ddc1100482deaf4f40
san-deep30 <sandeepmuhal1997@gmail.com>
2025-05-03 18:25:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
         var letters = '0123456789ABCDEF';
         var color = "#";
       for (var i = 0; i <6; i++) {
-        color += letters[Math.floor(Math.random() * 16)]
+        color += letters[Math.floor(Math.random() * 16)];
       }
       return color;
     }
